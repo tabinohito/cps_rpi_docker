0c928a803940c921ae09545b40124fb6d74e1a25
tabinohito <tako.taro.tf@tut.jp>
2024-01-10 00:04:43 +0900

    WIP

--- catkin_ws/src/wheel_tools/gyro/script/gyro_correction.py
+++ catkin_ws/src/wheel_tools/gyro/script/gyro_correction.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import rospy
+import time
 from sensor_msgs.msg import Joy
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist
@@ -9,22 +10,46 @@ import numpy as np
 from pyquaternion import Quaternion
 from scipy.spatial.transform import Rotation as R
 
-def quaternion_to_euler_zyx(q):
-    r = R.from_quat([q[0], q[1], q[2], q[3]])
-    return r.as_euler('zyx', degrees=True)
+class gyro_node():
+    def __init__(self):
+        self.sub_joystick = rospy.Subscriber('/joy/cmd_vel', Twist, self.get_joystick)
+        self.sub_imu = rospy.Subscriber('/imu/data', Imu, self.control)
+        self.pub_control = rospy.Publisher('/joy/gyro_correction/cmd_vel', Twist, queue_size=1)
 
-def control(msg, twist_pub):#joy to twist
-    q = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
-    yaw , roll , pitch = quaternion_to_euler_zyx(q)
-    print(yaw, roll, pitch)
+        self.joystick = None
+        self.initial_orientation = None
+        self.orientation_prev =None
+
+        self.kp = 0.1
+
+    def get_joystick(self,msg):
+        self.joystick = msg
+
+    def control(self, msg):#joy to twist
+        q = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        yaw , roll , pitch = self.quaternion_to_euler_zyx(q)
+        if self.initial_orientation is None:
+            self.initial_orientation = yaw , roll , pitch
+        else:
+            yaw -= self.initial_orientation[0]
+            roll -= self.initial_orientation[1]
+            pitch -= self.initial_orientation[2]
+
+            yaw_diff = yaw - self.orientation_prev[0]
+            
+
+        self.orientation_prev = yaw , roll , pitch        
+        print(yaw)
+
+    def quaternion_to_euler_zyx(self,q):
+        r = R.from_quat([q[0], q[1], q[2], q[3]])
+        return r.as_euler('zyx')
 
 if __name__ == '__main__':
-    #ノードの初期化
     rospy.init_node('gyro_correction')
-    #配信準備
-    twist_pub = rospy.Publisher('/joy/gyro_correction/cmd_vel', Twist, queue_size=1)
-    #購読
-    # rospy.Subscriber('/joy/cmd_vel', Twist, control, twist_pub)
-    rospy.Subscriber('/imu/data', Imu, control, twist_pub)
 
-    rospy.spin()
+    time.sleep(3.0)
+    node = gyro_node()
+
+    while not rospy.is_shutdown():
+        rospy.sleep(0.1)
